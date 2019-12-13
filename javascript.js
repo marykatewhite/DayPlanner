442bc6519919a497534d7c992060345b948a8cd0
MaryEtak <marykatewhite@gmail.com>
2019-12-12 20:05:04 -0500

    Got all the user inputs going into local storage, all local storage data populating onto the page. Just need to figure out color coding

--- javascript.js
+++ javascript.js
@@ -1,2 +1,31 @@
-let m = moment();
-$('#today').text(m)
+var submitBtn = $('#submit');
+
+function showTheTime() {
+    var m = moment().format('hh:mm:ss a');
+    document.getElementById("today").innerHTML = m;
+}
+
+showTheTime();
+setInterval(showTheTime, 250);
+
+
+// Actual functionality of the page
+
+var timeslot = $("#toDoItem").data("time", 9);
+
+var sls = function (event) {
+    // console.log(event.target.id)
+    // var id = event.target.id;
+    var timeSlot = $(event.target).attr("time");
+    console.log(event.target);
+
+    var itemName = "task" + timeSlot;
+
+    var task = $('#toDoItem').val();
+    // console.log(task)
+    // console.log(itemName)
+    localStorage.setItem(itemName, task)
+}
+
+
+submitBtn.click(sls);
